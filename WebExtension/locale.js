7de85a315f540a6f78f1c5eda224176a4bfb8076
Jeremy Schomery <jeremy.schomery@yahoo.com>
2018-01-15 17:59:06 +0330

    including 'sv' locale; based on #89

--- WebExtension/locale.js
+++ WebExtension/locale.js
@@ -2,10 +2,10 @@
 
 // localization
 [...document.querySelectorAll('[data-i18n]')].forEach(e => {
-  const value = e.dataset.i18nValue;
+  const values = e.dataset.i18nValue;
   const message = chrome.i18n.getMessage(e.dataset.i18n);
-  if (value) {
-    e.setAttribute(value, message);
+  if (values) {
+    values.split(',').forEach(value => e.setAttribute(value, message));
   }
   else {
     e.textContent = message;
